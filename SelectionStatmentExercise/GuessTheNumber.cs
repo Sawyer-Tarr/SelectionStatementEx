99f936a63d66d20be26ee38484a0fd76f1fceb7a
sawyer tarr <sawyertarr@outlook.com>
2023-02-03 14:43:03 -0600

    InitialCommit

---
+++ SelectionStatmentExercise/GuessTheNumber.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelectionStatmentExercise
+{
+    internal class GuessTheNumber
+    {
+        public static void FavNumber() 
+        {
+
+            var favNumber = 29;
+    
+            while (true)
+            {
+                var userInput = int.Parse(Console.ReadLine());
+
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("Come on. That is just too low. Please guess again.");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("Whoa there, you've gone waaaay to high. Why don't you give it another go?");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ SelectionStatmentExercise/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Exercise 1
+using SelectionStatmentExercise;
+
+Console.WriteLine("Please try to guess my favorite number between 1 and 50.");
+
+GuessTheNumber.FavNumber();
+
+// Exercise 2
+
+Console.WriteLine("What is your favorite school subject?");
+
+var subject = Console.ReadLine();
+
+switch (subject)
+{
+    case "Math":
+        Console.WriteLine("Oh nice! I really like math too!");
+        break;
+    case "Science":
+        Console.WriteLine("Nice! Did you know the midochondria is the powerhouse of the cell?");
+        break;
+    case "English":
+        Console.WriteLine("Yeah English wasn't my strong suit in school.");
+        break;
+    case "Lunch":
+        Console.WriteLine("Easily the best subject.");
+        break;
+    default:
+        Console.WriteLine("I'm not really familiar with that subject actually.");
+        break;
+}
